7d0c3661bd977aa36b205f8ecadc7fd241262d92
nusratsetu <nusratsetu00999@gmail.com>
2025-03-28 23:59:39 +0600

    new sections

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 let buttons = document.getElementsByClassName('complete-btn');
-for(let button of buttons){
-   button.addEventListener('click', function(){
+for (let button of buttons) {
+    button.addEventListener('click', function () {
         alert('Board updated sucessfully!');
     });
-
-
-    const taaskm = document.getElementById('task').innerText;
-    const convertTask = parseInt(taaskm)
-    const difference = convertTask - taaskm;
-    const taask = document.getElementById('task').innerText = difference
-
-    button.addEventListener('click', function(){
-
-    })
-    
 }
+
+let button2 = document.getElementsByClassName('complete-btn');
+let taskNum = 6;
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        if (taskNum > 0) {
+            taskNum--;
+            document.getElementById('task').innerText = taskNum;
+        }
+    })
+
+    let navNum = 23;
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            navNum++;
+            document.getElementById('navNumber').innerText = navNum;
+            this.disabled = true;
+            this.style.backgroundColor = '#CED6FF';
+        })
+    }
+}
+
+
+
+
+
+
+
